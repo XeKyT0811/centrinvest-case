7080f56f0b71863cd311c635b16536a9eb00f4f3
XeKyT0811 <hammerbanned@gmail.com>
2026-05-19 00:21:48 +0300

    Added auth endpoint placeholder

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET'])
 def test():
     return render_template("index.html")
+
+@app.route("/auth", methods = ['GET'])
+def test():
+    return render_template("auth.html")
 
 if (__name__ == '__main__'):
     app.run()
