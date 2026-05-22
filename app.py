5a719612c8e7fd028416835f266471b3ad621dca
XeKyT0811 <hammerbanned@gmail.com>
2026-05-22 15:42:12 +0300

    Core layout, main and AI pages almost complete

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import *
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
-def test():
+def get_index():
     return render_template("index.html")
 
 @app.route("/auth", methods = ['GET'])
-def test():
+def auth_page():
     return render_template("auth.html")
 
 if (__name__ == '__main__'):
-    app.run()
+    app.run(debug=True)
