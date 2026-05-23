982e759dbc350638b3b49d7f59e93c299fbbb1e9
Alex <magiclood66@gmail.com>
2026-05-23 18:43:07 +0300

    added backend prototype

--- app.py
+++ app.py
@@ -1,14 +1,63 @@
 from flask import *
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+from database import db, User, Sprint, AI_model, AI_usage, login_manager
+from flask_login import login_user, current_user, logout_user, login_required
+from werkzeug.security import check_password_hash
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET'])
-def get_index():
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'auth_page'
+
+@app.route("/")
+@login_required
+def index():
     return render_template("index.html")
 
-@app.route("/auth", methods = ['GET'])
+@app.route("/auth")
 def auth_page():
     return render_template("auth.html")
 
+@app.route("/login", methods = ["POST"])
+def login():
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+
+    user = User.query.filter_by(login=username).first()
+
+    if (user):
+        if (check_password_hash(user.password_hash, password)):
+            login_user(user)
+            return jsonify({"success": True}), 200
+        else:
+            return jsonify({"success": False}), 401
+    else:
+        return jsonify({"success": False}), 404
+
+@app.route("/logout", methods=["POST", "GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"success": True}), 200
+
+@app.route("/dashboard", methods=["GET"])
+@login_required
+def dashboard_data():
+    return
+
+
 if (__name__ == '__main__'):
-    app.run()
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,53 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth_page'
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    sprints = db.relationship('Sprint', backref='user', lazy=True)
+
+
+class Sprint(db.Model):
+    __tablename__ = 'sprints'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    tasks_total = db.Column(db.Integer, nullable=False)
+    tasks_completed = db.Column(db.Integer, nullable=False)
+    bugs = db.Column(db.Integer, nullable=False)
+    returned_bugs = db.Column(db.Integer, nullable=False)
+    releases = db.Column(db.Integer, nullable=False)
+
+    ai_usage_days = db.relationship('AI_usage', backref='sprint', lazy=True)
+
+
+class AI_model(db.Model):
+    __tablename__ = 'ai_models'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    cost = db.Column(db.Float, nullable=False)
+
+
+class AI_usage(db.Model):
+    __tablename__ = 'ai_usage'
+
+    id = db.Column(db.Integer, primary_key=True)
+    sprint_id = db.Column(db.Integer, db.ForeignKey("sprints.id"), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    model_id = db.Column(db.Integer, db.ForeignKey("ai_models.id"), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
